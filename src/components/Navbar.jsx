fbce988d4df79f00ad04a1815e5ecc9c7ac874a5
AkshitaBajaj2112 <AkshitaBajajCa@gmail.com>
2024-02-19 16:30:23 -0500

    register user with firebase auth

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
+import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+
+  return (
+    <footer className="navbar">
+      <nav className="navbarNav">
+        <ul className="navbarListItems">
+          <li className="navbarListItem" onClick={() => navigate("/")}>
+            <ExploreIcon
+              fill={pathatchRoute("/") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathatchRoute("/")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Explore
+            </p>
+          </li>
+          <li className="navbarListItem" onClick={() => navigate("/offers")}>
+            <OfferIcon
+              fill={pathatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathatchRoute("/offers")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Offer
+            </p>
+          </li>
+          <li className="navbarListItem" onClick={() => navigate("/profile")}>
+            <PersonOutlineIcon
+              fill={pathatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathatchRoute("/profile")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Profile
+            </p>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Navbar;
